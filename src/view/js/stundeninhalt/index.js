e79d29467f55ad96171b7a2c4f42e6adb147c0b5
Oliver Ochoa Samsung Notebook <oliver.ochoa@hotmail.de>
2017-11-10 14:31:58 +0100

    js

--- src/view/js/stundeninhalt/index.js
+++ src/view/js/stundeninhalt/index.js
@@ -1,13 +1,67 @@
 $('#delete').click(function (e) {
     return confirm("Wollen Sie wirklich den Stundeninhalt löschen");
 });
-
+// YYYY-MM-DD
 //Oliver Script ^^
 document.addEventListener("DOMContentLoaded", function() {
-	var scrollWeeks = {
+	var scrollAjax = {
+		buttons: document.querySelectorAll(".js-scroll"),
+		init: function(){
+			this.addListener();
+		},
+		call: function(data){
+			$.ajax({
+			  type: "POST",
+			  url: "do-get-by-date-range.php",
+			  data: {
+				  startDate: data.monday,
+				  endDate: data.friday
+			  },
+			  success: function(){
+				  console.log("test");
+			  }
+		  	});
+		},
+		addListener:function(){
+			Array.prototype.forEach.call(this.buttons, function(button) {
+				button.addEventListener("click",function(){
+					scrollAjax.call(scrollAjax.nextWeek());
+				});
+			});
+		},
+		nextWeek: function(){
+			function formatDate(date) {
+			    var d = new Date(date),
+			        month = '' + (d.getMonth() + 1),
+			        day = '' + d.getDate(),
+			        year = d.getFullYear();
+
+			    if (month.length < 2) month = '0' + month;
+			    if (day.length < 2) day = '0' + day;
+			    return [year, month, day].join('-');
+			}
+			var today = new Date();
+			var diff = today.getDate() - today.getDay() + 1;
+			if (today.getDay() == 0){
+				diff -= 7;
+			}else{
+				diff += 7;
+			}
+			var monday = new Date(today.setDate(diff));
+			var friday = new Date(monday);
+			friday.setDate(friday.getDate() + (1 + 4 - friday.getDay()) % 7);
+			return {
+				"monday": formatDate(monday),
+				"friday": formatDate(friday)
+			};
+		}
+	}
+	scrollAjax.init();
+	/*var scrollWeeks = {
 		scrollPostion: Number(),
 		buttons: document.querySelectorAll(".js-scroll"),
 		data: Array(),
+		type: String(),
 		init: function(data){
 			this.data = this.sortByDate(data);
 			this.addListener();
@@ -24,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function() {
 			Array.prototype.forEach.call(this.buttons, function(button) {
 				button.addEventListener("click",function(){
 					alert(this.getAttribute("id"));
-					scrollWeeks.build()
+					scrollWeeks.build();
 				});
 			});
 		},
@@ -53,5 +107,5 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 	if(document.querySelector("#weekDate")){
 		scrollWeeks.init(JSON.parse(document.querySelector("#weekDate").getAttribute("data-weeks")));
-	}
+	}*/
 });
