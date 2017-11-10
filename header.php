8cc03b4bb4ba15cf174fe49a484158adf41678fd
TobiasWohlers <tobias.wohlers@hitmeister.de>
2017-11-10 20:52:44 +0100

    change order of nav links

--- header.php
+++ header.php
@@ -12,12 +12,12 @@
 			<i class="fa fa-bars" aria-hidden="true" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true"
 			   aria-expanded="true"></i>
 			<ul class="dropdown-menu dropdown-pull-right" aria-labelledby="dropdownMenu1">
+				<li><a href="../stundeninhalt/index.php">Stundeninhalt</a></li>
 				<li><a href="../bewertungsbogen/index.php">Bewertungsbogen</a></li>
 				<li><a href="../klassen/index.php">Klassen</a></li>
 				<li><a href="../lehrer/index.php">Lehrer</a></li>
 				<li><a href="../schueler/index.php">Sch&uuml;ler</a></li>
 				<li><a href="../stunden/index.php">Fächer</a></li>
-				<li><a href="../stundeninhalt/index.php">Stundeninhalt</a></li>
 				<li><a href="../login/do-logout.php">Ausloggen</a></li>
 			</ul>
 		</div>
